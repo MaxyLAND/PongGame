4b15814ffb037fc0377b30614a35bb0d680272ff
MaxyLAND <maxylandbuzon@gmail.com>
2021-10-30 16:36:24 +0200

    new features and visually upgraded

--- js/main.js
+++ js/main.js
@@ -4,23 +4,54 @@
 var computerLevel = 0.03;
 var angleRad = 0;
 
+var paddleUser = new Image(50, 200);
+paddleUser.src = "./img/paddleUser.png";
+var paddleBot = new Image(50, 200);
+paddleBot.src = "./img/paddleBot.png";
+var paddleUserShadow = new Image(50, 200);
+paddleUserShadow.src = "./img/paddleUserShadow.png";
+var paddleBotShadow = new Image(50, 200);
+paddleBotShadow.src = "./img/paddleBotShadow.png";
+var ballShadow = new Image(50, 50);
+ballShadow.src = "./img/ballShadow.png";
+var background = new Image(500, 500);
+background.src = "./img/background.png";
+var gameShadow = new Image(1280, 720);
+gameShadow.src = "./img/gameShadow.png";
+var fade = new Image(250, 250);
+fade.src = "./img/FadeNegro.png";
+
+var bg1x = -465;
+var bg2x = 0;
+var bg3x = 465;
+
+/*                              IMAGEN:
+base_image = new Image();
+base_image.src = 'img/base.png';
+base_image.onload = () => ctx.drawImage(base_image, 0, 0);
+*/
 
 //Coger elemento Canvas del HTML
 const canvas = document.getElementById("pong"); //pasarlo a variable const
-
 const ctx = canvas.getContext("2d"); //method returns a drawing context on the canvas
+
+const canvasUserPU = document.getElementById("UserPowerUpBar");
+const canvasBotPU = document.getElementById("BotPowerUpBar");
+
+const ctxUser = canvasUserPU.getContext("2d");
+const ctxBot = canvasBotPU.getContext("2d");
 
 
 //Funciones de dibujo (Draw Functions)
 
 //RECTÁNGULO O CUADRADO
-function drawRect(x, y, w, h, color) { // Dibujar con: Eje X Eje Y (TopLeft corner), Weith, Height, color
+function drawRect(x, y, w, h, color, ctx) { // Dibujar con: Eje X Eje Y (TopLeft corner), Weith, Height, color
     ctx.fillStyle = color; //Pintarlo de color
     ctx.fillRect(x, y, w, h) //Rellenar el rectángulo para que sea visible y no solo líneas
 }
 
 //CÍRCULO U ÓVALO                           (el eje (donde X e Y es 0) de la bola es el centro, y no la esquina superior izquierda)
-function drawCircle(x, y, r, color) {
+function drawCircle(x, y, r, color, ctx) {
     ctx.fillStyle = color; //Pintarlo de color
     ctx.beginPath(); //Comenzar la creación del radio del círculo
     ctx.arc(x, y, r, 0, Math.PI * 2 /*360º*/ , false); //X, Y, radius, start angle, end angle, direction (false: left, true: right)
@@ -29,11 +60,13 @@ function drawCircle(x, y, r, color) {
 }
 
 //TEXTO
-function drawText(text, x, y, color) {
+function drawText(text, x, y, color, ctx) {
     ctx.fillStyle = color; //Pintarlo de color
     ctx.font = "75px fantasy"; //Asignarle una fuente
     ctx.fillText(text, x, y); //Crear el texto en los ejes X Y seleccionados
 }
+
+
 
 //Funciones de movimiento
 
@@ -57,9 +90,9 @@ function render() {
 const user = {
     "x": 20,
     "y": (canvas.height / 2 - 100 / 2) + 20,
-    "width": 20,
+    "width": 40,
     "height": 100,
-    "color": "white",
+    "color": "black",
     "score": 0,
     "top": 0,
     "bottom": 0,
@@ -69,11 +102,11 @@ const user = {
 
 //La pala del bot
 const bot = {
-    "x": canvas.width - 40,
+    "x": canvas.width - 60,
     "y": (canvas.height / 2 - 100 / 2) + 80,
-    "width": 20,
+    "width": 40,
     "height": 100,
-    "color": "white",
+    "color": "black",
     "score": 0,
     "top": 0,
     "bottom": 0,
@@ -85,15 +118,15 @@ const bot = {
 const red = {
     "x": canvas.width / 2 - 2 / 2,
     "y": 0,
-    "width": 3,
-    "height": 10,
+    "width": 6,
+    "height": 25,
     "color": "gray"
 }
 
 //Funcion para dibujar la red
-function drawRed() {
-    for (let i = 0; i < canvas.height; i += 18) {
-        drawRect(red.x, red.y + i, red.width, red.height, red.color);
+function drawRed(ctx) {
+    for (let i = 0; i < canvas.height; i += red.height * 1.5) {
+        drawRect(red.x, red.y + i, red.width, red.height, "rgba(0, 0, 0, 0.4)", ctx);
     }
 }
 
@@ -101,7 +134,7 @@ function drawRed() {
 const ball = {
     "x": canvas.width / 2,
     "y": canvas.height / 2,
-    "radius": 10,
+    "radius": 15,
     "speed": 15,
     "velocityX": 10,
     "velocityY": 10,
@@ -111,6 +144,25 @@ const ball = {
     "left": 0,
     "right": 0
 }
+
+//PowerUp User bar
+const UserPU = {
+    "x": 0,
+    "y": 0,
+    "width": canvasUserPU.width,
+    "height": canvasUserPU.height,
+    "color": "black"
+}
+
+//PowerUp Bot bar
+const BotPU = {
+    "x": 0,
+    "y": 0,
+    "width": canvasBotPU.width,
+    "height": canvasBotPU.height,
+    "color": "black"
+}
+
 
 //Key detecter
 document.onkeydown = checkKey;
@@ -122,13 +174,7 @@ function checkKey(e) {
 
     console.log(moveEff);
     if (!moveEff) {
-        if (e.keyCode == "38" || e.keyCode == "87") { // up arrow / w
-
-        } else if (e.keyCode == "40" || e.keyCode == "83") { // down arrow
-
-        } else if (e.keyCode == "37" || e.keyCode == "65") { // left arrow
-
-        } else if (e.keyCode == "39" || e.keyCode == "68") { // right arrow
+        if (e.keyCode == "32") { //Space
             moveEff = true;
             effBool = false;
             eff2Bool = false;
@@ -137,6 +183,14 @@ function checkKey(e) {
             oldPos = user.x + 50;
             func1 = setInterval(playerMoveEff, 1000 / fps);
             func2 = setInterval(waitForPowerUp, 1000 / fps);
+        } else if (e.keyCode == "38" || e.keyCode == "87") { // up arrow / w
+
+        } else if (e.keyCode == "40" || e.keyCode == "83") { // down arrow
+
+        } else if (e.keyCode == "37" || e.keyCode == "65") { // left arrow
+            alert(bg1x);
+        } else if (e.keyCode == "39" || e.keyCode == "68") { // right arrow
+
         }
     }
 }
@@ -177,28 +231,112 @@ function waitForPowerUp() {
         moveEff = false;
         clearInterval(func2);
     }
+    var i = (f / 240);
+    UserPU.width = i * canvasUserPU.width;
     f++;
+}
+
+
+//Empuje efecto (BOT)
+let i2 = 0;
+let f2 = 0;
+let func12 = null;
+let func22 = null;
+let moveEff2 = false;
+let effBool2 = false;
+let eff2Bool2 = false;
+let oldPos2 = 0;
+
+//Funcion para ejecutar el movimiento de tu jugador (BOT)               
+function botMoveEff() {
+    i2++
+
+    if (i2 > 120) {
+        clearInterval(func12);
+    } else if (i2 > 8) {
+        bot.x += ((canvas.width - 60) - bot.x) * 0.05;
+    } else {
+        bot.x += (oldPos2 - bot.x) * 0.5;
+        if (!effBool2) {
+            effBool2 = true;
+            eff2Bool2 = true;
+            ball.speed += 15;
+            console.log("EFFECT APPLIED.");
+        }
+    }
+}
+
+//Funcion para esperar cierto tiempo para volver a usar una habilidad (BOT)
+function waitForPowerUpBot() {
+    if (f2 > 240) {
+        moveEff2 = false;
+        clearInterval(func22);
+    }
+    var i2 = (f2 / 240);
+    BotPU.width = i2 * canvasBotPU.width;
+    f2++;
 }
 
 
 //Render the game
 function render() {
-    //Fondo negro
-    drawRect(0, 0, canvas.width, canvas.height, "black");
+    //Fondo
+    drawRect(0, 0, canvas.width, canvas.height, "#EDFF86", ctx);
+
+    //BG lines
+    ctx.drawImage(background, bg1x, 0, 500, 500);
+    ctx.drawImage(background, bg2x, 0, 500, 500);
+    ctx.drawImage(background, bg3x, 0, 500, 500);
+
+    //Scores Shadow
+    drawText(user.score, (canvas.width / 4) + 6, (canvas.height / 5) + 6, "rgba(0, 0, 0, 0.4)", ctx);
+    drawText(bot.score, (3 * (canvas.width / 4)) + 6, (canvas.height / 5) + 6, "rgba(0, 0, 0, 0.4)", ctx);
 
     //Scores
-    drawText(user.score, canvas.width / 4, canvas.height / 5, "white");
-    drawText(bot.score, 3 * (canvas.width / 4), canvas.height / 5, "white");
+    drawText(user.score, canvas.width / 4, canvas.height / 5, "white", ctx);
+    drawText(bot.score, 3 * (canvas.width / 4), canvas.height / 5, "white", ctx);
 
     //Red
-    drawRed();
+    drawRed(ctx);
 
     //Palas
-    drawRect(user.x, user.y, user.width, user.height, user.color);
-    drawRect(bot.x, bot.y, bot.width, bot.height, bot.color);
+    drawRect(user.x, user.y, user.width, user.height, "#00000000", ctx);
+    drawRect(bot.x, bot.y, bot.width, bot.height, "#00000000", ctx);
+
+    //Bola Shadow
+    drawCircle(ball.x + 8, ball.y + 8, ball.radius, "rgba(0, 0, 0, 0.4)", ctx);
 
     //Bola
-    drawCircle(ball.x, ball.y, ball.radius, ball.color);
+    drawCircle(ball.x, ball.y, ball.radius, "#62B9FF", ctx);
+
+    //PowerUp Bars
+    //User
+    drawRect(0, 0, canvasUserPU.width, canvasUserPU.height, "white", ctxUser);
+    drawRect(UserPU.x, UserPU.y, UserPU.width, UserPU.height, UserPU.color, ctxUser);
+    if (UserPU.width >= canvasUserPU.width)
+        drawText("Space", canvasUserPU.width / 6 * 1.15, +70, "white", ctxUser);
+
+    //Bot
+    drawRect(0, 0, canvasBotPU.width, canvasBotPU.height, "white", ctxBot);
+    drawRect(BotPU.x, BotPU.y, BotPU.width, BotPU.height, BotPU.color, ctxBot);
+    if (BotPU.width >= canvasBotPU.width)
+        drawText("Space", canvasBotPU.width / 6 * 1.15, +70, "white", ctxBot);
+
+    //IMAGENES
+
+    //User/bot 3D shadows
+    ctx.drawImage(paddleUserShadow, user.x + 8, user.y + 8, user.width, user.height);
+    ctx.drawImage(paddleBotShadow, bot.x + 8, bot.y + 8, bot.width, bot.height);
+
+    //User/bot paddle shadows
+    //ctx.drawImage(fade, bot.x - 10, bot.y - 20, bot.width + 20, bot.height + 40);
+    //ctx.drawImage(fade, user.x - 10, user.y - 20, user.width + 20, user.height + 40);
+
+    //User/Bot paddles
+    ctx.drawImage(paddleUser, user.x, user.y, user.width, user.height);
+    ctx.drawImage(paddleBot, bot.x, bot.y, bot.width, bot.height);
+
+    ctx.drawImage(gameShadow, 0, 0, canvas.width, canvas.height);
 }
 
 
@@ -216,7 +354,19 @@ function collision(b, p) { //B: ball   P: player
     b.left = b.x - b.radius; //calcular el left de b
     b.right = b.x + b.radius; //calcular el right de b
 
-
+    //Bot PowerUp collision
+    if (!moveEff2 && (ball.bottom > bot.top + 10 && ball.right > bot.left - 40 && ball.left < bot.right + 40 && ball.top < bot.bottom - 10)) {
+        if (BotPU.width >= canvasBotPU.width) {
+            moveEff2 = true;
+            effBool2 = false;
+            eff2Bool2 = false;
+            i2 = 0;
+            f2 = 0;
+            oldPos2 = bot.x - 50;
+            func12 = setInterval(botMoveEff, 1000 / fps);
+            func22 = setInterval(waitForPowerUpBot, 1000 / fps);
+        }
+    }
 
     //saber si b impacta con el p
     if (b.bottom > p.top && b.right > p.left && b.left < p.right && b.top < p.bottom) {
@@ -227,8 +377,14 @@ function collision(b, p) { //B: ball   P: player
         //Calcular el ángulo por el nuevo en el que el círculo debería moverse (si collidePoint es 0, se moverá a 0 grados, si es 1, se moverá a 45, etc)
         angleRad = (Math.PI / 4) * collidePoint; //Math.PI / 4 son 45 grados. * 2 son 360.
 
+
+
         //Calcular ángulos depende de dónde impacte con el player devolviendo valores VelocityX y VelocityY
         if (ball.x < canvas.width / 2) { //Comprobar si la ball está en la izquierda o en la derecha de la pantalla para invertir el movimiento en X.
+            if (eff2Bool2 == true) {
+                eff2Bool2 = false;
+                ball.speed -= 15;
+            }
             b.velocityX = b.speed * Math.cos(angleRad); //Convertir la velocidad X de la ball en la velocidad de la ball * el coseno del angleRad.
         } else {
             if (eff2Bool) {
@@ -244,16 +400,17 @@ function collision(b, p) { //B: ball   P: player
 }
 
 //Función para reiniciar los valores de la ball e invertir su movimiento en el eje X
-function resetBall() {
+function resetBall(b) {
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
     ball.speed = 15;
-    ball.velocityX = -ball.velocityX;
+    ball.velocityX = ball.velocityX * -1;
     eff2Bool = false;
+    eff2Bool2 = false;
     console.log(ball.speed);
 
     //Update VelocityX VelocityY
-    if (ball.x < canvas.width / 2)
+    if (b)
         ball.velocityX = ball.speed * Math.cos(angleRad);
     else
         ball.velocityX = -1 * (ball.speed * Math.cos(angleRad));
@@ -276,10 +433,10 @@ function update() {
     //Comprobar si la bola está muy arriba o muy abajo para que rebote con las paredes
     if (ball.y + ball.radius > canvas.height) {
         ball.velocityY = -ball.velocityY;
-        ball.velocityY -= 0.3;
+        ball.velocityY -= 0.6;
     } else if (ball.y - ball.radius < 0) {
         ball.velocityY = -ball.velocityY;
-        ball.velocityY += 0.3;
+        ball.velocityY += 0.6;
     }
 
     //Comprobar si la bola está en la izquierda o en la derecha para llamar a la función collision de user o bot.
@@ -292,13 +449,15 @@ function update() {
     //Comprobar si la ball ha salido de los bordes del mapa. Si lo ha hecho, sumar la puntuación al contrincante y reiniciar la posición de esta.
     if (ball.x < 0) {
         bot.score++;
-        computerLevel -= 0.002;
-        resetBall();
+        if (computerLevel > 0.01)
+            computerLevel -= 0.002;
+        resetBall(true);
     }
     if (ball.x > canvas.width) {
         user.score++;
-        computerLevel += 0.01;
-        resetBall();
+        if (computerLevel < 0.99)
+            computerLevel += 0.01;
+        resetBall(false);
     }
 
     //Comprobar la posición del ratón y ejecutar la función "movePaddle"
@@ -306,6 +465,18 @@ function update() {
 
     //Mover el bot haciendo uso de la posición de la ball y la suya propia, multiplicado por un nivel dinámico del bot.
     bot.y += (ball.y - (bot.y + bot.height / 2)) * computerLevel;
+
+    //Background things
+    bg1x++;
+    bg2x++;
+    bg3x++;
+
+    if (bg1x >= 930)
+        bg1x = -450;
+    else if (bg2x >= 930)
+        bg2x = -450;
+    else if (bg3x >= 930)
+        bg3x = -450;
 }
 
 //Game function
